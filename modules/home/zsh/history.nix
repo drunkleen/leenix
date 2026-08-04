9e7734417c3f3519ebcc8a52365c07c5bee5780d
DrunkLeen <snape@drunkleen.com>
2026-08-04 17:25:35 +0200

    improve Zsh history search

--- modules/home/zsh/history.nix
+++ modules/home/zsh/history.nix
@@ -1,10 +1,27 @@
 {
-  programs.zsh.history = {
-    path = "$HOME/.local/share/zsh/history";
-    size = 10000;
-    save = 10000;
-    ignoreDups = true;
-    ignoreSpace = true;
-    share = true;
+  programs.zsh = {
+    history = {
+      path = "$HOME/.local/share/zsh/history";
+      size = 50000;
+      save = 50000;
+
+      ignoreDups = true;
+      ignoreSpace = true;
+      share = true;
+
+      expireDuplicatesFirst = true;
+      extended = true;
+    };
+
+    initContent = ''
+      autoload -Uz up-line-or-beginning-search
+      autoload -Uz down-line-or-beginning-search
+
+      zle -N up-line-or-beginning-search
+      zle -N down-line-or-beginning-search
+
+      bindkey "^[[A" up-line-or-beginning-search
+      bindkey "^[[B" down-line-or-beginning-search
+    '';
   };
 }
