545b681c354c551f5c6c720b3e9028ec2590aa23
DrunkLeen <snape@drunkleen.com>
2026-08-04 22:17:18 +0200

    configure PipeWire audio stack

--- modules/nixos/hardware/audio.nix
+++ modules/nixos/hardware/audio.nix
@@ -1,4 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+    pulseaudio
+    pavucontrol
+    pwvucontrol
+    crosspipe
+  ];
 }
