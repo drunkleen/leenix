c5be28f516f5264600bd50518491ac9e7c957475
DrunkLeen <snape@drunkleen.com>
2026-08-05 00:32:48 +0200

    configure desktop cursor theme

--- modules/home/themes/cursor.nix
+++ modules/home/themes/cursor.nix
@@ -1,1 +1,13 @@
-{}
+{ pkgs, ... }:
+
+{
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+
+    name = "Bibata-Modern-Ice";
+    package = pkgs.bibata-cursors;
+
+    size = 24;
+  };
+}
