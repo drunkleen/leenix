a54ffb6fc9f6e4be9a087d7c70ee3c2abe7056cb
DrunkLeen <snape@drunkleen.com>
2026-08-04 22:34:58 +0200

    configure power management

--- modules/nixos/hardware/power.nix
+++ modules/nixos/hardware/power.nix
@@ -1,4 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  services.power-profiles-daemon.enable = true;
+
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    powertop
+  ];
 }
