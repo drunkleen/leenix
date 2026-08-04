3570e2241cdd710bfa19b73d970899f6587be729
DrunkLeen <snape@drunkleen.com>
2026-08-04 21:44:07 +0200

    enable Intel microcode and firmware

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./boot
+    ./hardware
     ./networking
     ./services
     ./storage
---
+++ modules/nixos/hardware/default.nix
@@ -0,0 +1,8 @@
+{ config, lib, ... }:
+
+{
+  hardware.enableRedistributableFirmware = true;
+
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
