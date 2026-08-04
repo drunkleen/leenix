3173cfbd9972fada53ccd6c77e1c84fed357b202
DrunkLeen <snape@drunkleen.com>
2026-08-04 23:15:50 +0200

    configure NVIDIA PRIME offload

--- modules/nixos/hardware/graphics.nix
+++ modules/nixos/hardware/graphics.nix
@@ -1,4 +1,38 @@
-{ ... }:
+{ config, lib, ... }:
 
 {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+    ];
+
+  services.xserver.videoDrivers = [
+    "modesetting"
+    "nvidia"
+  ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    nvidiaSettings = true;
+
+    prime = {
+      intelBusId = "PCI:0@0:2:0";
+      nvidiaBusId = "PCI:1@0:0:0";
+
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+    };
+  };
 }
