caccbaab4d9ff90eb639a2bc272b60dc808f4d35
DrunkLeen <snape@drunkleen.com>
2026-08-05 13:04:39 +0200

    use Hyprlang for initial Hyprland configuration

--- modules/home/hyprland/package.nix
+++ modules/home/hyprland/package.nix
@@ -3,6 +3,15 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
+
+    # Hyprland is installed system-wide by the NixOS module.
+    package = null;
+    portalPackage = null;
+
+    # Our current modular settings use Hyprlang syntax.
+    configType = "hyprlang";
+
+    # UWSM manages the graphical session.
     systemd.enable = false;
   };
 }
