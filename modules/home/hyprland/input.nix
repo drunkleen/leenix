11d172498147ee901fd88952b3ab8c376a118871
DrunkLeen <snape@drunkleen.com>
2026-08-06 01:38:58 +0200

    improve Hyprland input experience

--- modules/home/hyprland/input.nix
+++ modules/home/hyprland/input.nix
@@ -1,13 +1,31 @@
 {
-  wayland.windowManager.hyprland.settings.input = {
-    kb_layout = "us";
-    follow_mouse = 1;
-    sensitivity = 0;
+  wayland.windowManager.hyprland.settings = {
+    input = {
+      kb_layout = "us";
 
-    touchpad = {
-      natural_scroll = true;
-      tap-to-click = true;
-      disable_while_typing = true;
+      follow_mouse = 1;
+      sensitivity = 0;
+
+      repeat_delay = 250;
+      repeat_rate = 35;
+
+      accel_profile = "flat";
+
+      touchpad = {
+        natural_scroll = true;
+        tap-to-click = true;
+        disable_while_typing = true;
+        clickfinger_behavior = true;
+      };
+    };
+
+    gesture = [
+      "3, horizontal, workspace"
+    ];
+
+    gestures = {
+      workspace_swipe_invert = false;
+      workspace_swipe_forever = true;
     };
   };
 }
