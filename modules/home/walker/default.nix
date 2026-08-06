dec020bbd5e8acfd5af1ec9d53d44f097cc78956
DrunkLeen <snape@drunkleen.com>
2026-08-06 02:05:31 +0200

    customize Walker behavior

--- modules/home/walker/default.nix
+++ modules/home/walker/default.nix
@@ -8,5 +8,56 @@
   programs.walker = {
     enable = true;
     runAsService = true;
+
+    config = {
+      force_keyboard_focus = true;
+      selection_wrap = true;
+      hide_action_hints = true;
+
+      placeholders.default = {
+        input = " Search...";
+        list = "No Results";
+      };
+
+      keybinds.quick_activate = [ ];
+
+      columns.symbols = 1;
+
+      providers = {
+        max_results = 256;
+
+        default = [
+          "desktopapplications"
+          "websearch"
+        ];
+
+        prefixes = [
+          {
+            prefix = "/";
+            provider = "providerlist";
+          }
+          {
+            prefix = ".";
+            provider = "files";
+          }
+          {
+            prefix = ":";
+            provider = "symbols";
+          }
+          {
+            prefix = "=";
+            provider = "calc";
+          }
+          {
+            prefix = "@";
+            provider = "websearch";
+          }
+          {
+            prefix = "$";
+            provider = "clipboard";
+          }
+        ];
+      };
+    };
   };
 }
