17ba9732ae476d82ad5cfc4b05c5aaea7f8a1399
DrunkLeen <snape@drunkleen.com>
2026-08-06 18:17:56 +0200

    apply leenium palette to walker

--- modules/home/walker/default.nix
+++ modules/home/walker/default.nix
@@ -1,5 +1,34 @@
 { inputs, ... }:
 
+let
+  palette = import ../../../lib/leenium.nix;
+
+  style =
+    builtins.replaceStrings
+      [
+        "@LEENIUM_FOREGROUND@"
+        "@LEENIUM_BACKGROUND@"
+        "@LEENIUM_TEAL@"
+        "@LEENIUM_POPUP@"
+        "@LEENIUM_DISABLED@"
+        "@LEENIUM_SELECTION@"
+        "@LEENIUM_BRIGHT@"
+        "@LEENIUM_CYAN@"
+        "@LEENIUM_SECONDARY@"
+      ]
+      [
+        palette.neutral.foreground
+        palette.background.main
+        palette.accent.teal
+        palette.background.popup
+        palette.neutral.disabled
+        palette.background.selection
+        palette.neutral.bright
+        palette.accent.cyan
+        palette.neutral.secondary
+      ]
+      (builtins.readFile ./theme/style.css);
+in
 {
   imports = [
     inputs.walker.homeManagerModules.default
@@ -10,7 +39,7 @@
     runAsService = true;
 
     themes."leenium.hyprland" = {
-      style = builtins.readFile ./theme/style.css;
+      inherit style;
       layouts = {
         layout = builtins.readFile ./theme/layout.xml;
       };
