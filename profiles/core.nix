a50ccff37af74013507dd2db1b234107441588b3
DrunkLeen <snape@drunkleen.com>
2026-08-06 17:43:05 +0200

    install agenix cli

--- profiles/core.nix
+++ profiles/core.nix
@@ -1,7 +1,16 @@
-{ vars, ... }:
+{
+  inputs,
+  pkgs,
+  vars,
+  ...
+}:
 
 {
   nix.settings.warn-dirty = false;
+
+  environment.systemPackages = [
+    inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
 
   time.timeZone = vars.timezone;
   i18n.defaultLocale = vars.locale;
