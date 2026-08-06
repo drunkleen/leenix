83b9b57a3b1164514fe60bb55bcfbc0fd15d3644
DrunkLeen <snape@drunkleen.com>
2026-08-06 19:28:48 +0200

    apply leenium palette to zsh suggestions

--- modules/home/zsh/autosuggestions.nix
+++ modules/home/zsh/autosuggestions.nix
@@ -1,5 +1,11 @@
+let
+  leenium = import ../../../lib/leenium.nix;
+in
 {
   programs.zsh = {
-    autosuggestion.enable = true;
+    autosuggestion = {
+      enable = true;
+      highlight = "fg=${leenium.neutral.muted}";
+    };
   };
 }
