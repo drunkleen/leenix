c95aaf07b7bdb36637b83d16bae1cd9b432ed11c
DrunkLeen <snape@drunkleen.com>
2026-08-06 23:37:12 +0200

    configure hyprland portal backends

--- modules/nixos/desktop/foundation/portal.nix
+++ modules/nixos/desktop/foundation/portal.nix
@@ -10,10 +10,15 @@
       pkgs.xdg-desktop-portal-gtk
     ];
 
-    config = {
-      common = {
-        default = "*";
-      };
+    config.hyprland = {
+      default = [
+        "hyprland"
+        "gtk"
+      ];
+
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+      "org.freedesktop.impl.portal.AppChooser" = [ "gtk" ];
+      "org.freedesktop.impl.portal.Settings" = [ "gtk" ];
     };
   };
 }
