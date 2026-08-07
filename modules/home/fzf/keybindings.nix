e83f376c192326c48d04e46fba313702ff24425f
DrunkLeen <snape@drunkleen.com>
2026-08-07 18:40:42 +0200

    .

--- modules/home/fzf/keybindings.nix
+++ modules/home/fzf/keybindings.nix
@@ -2,11 +2,9 @@
   programs.fzf = {
     enableZshIntegration = true;
 
-    changeDirWidgetCommand =
-      "fd --type d --hidden --follow --exclude .git";
+    changeDirWidgetCommand = "fd --type d --hidden --follow --exclude .git";
 
-    fileWidgetCommand =
-      "fd --type f --hidden --follow --exclude .git";
+    fileWidgetCommand = "fd --type f --hidden --follow --exclude .git";
 
     historyWidgetOptions = [
       "--sort"
--- modules/home/hyprland/uwsm.nix
+++ modules/home/hyprland/uwsm.nix
@@ -3,7 +3,7 @@
 {
   options.leenix.fallbackSession.target = lib.mkOption {
     type = lib.types.str;
-    default = "wayland-session@leenix\\x2duwsm.desktop.target";
+    default = "wayland-session@hyprland\\x2duwsm.desktop.target";
     description = "UWSM target that owns the retained Hyprland fallback desktop services.";
   };
 }
