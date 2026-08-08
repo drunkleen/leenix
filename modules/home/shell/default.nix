e5484d7ca2df906523e42534f978e42390cf98e8
DrunkLeen <snape@drunkleen.com>
2026-08-08 15:32:51 +0200

    refactor: remove shell activation hack

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -11,14 +11,4 @@
   programs.starship = {
     enable = true;
   };
-
-  home.activation.setDefaultShell =
-    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    current_shell="$(getent passwd "$USER" | cut -d: -f7)"
-
-    if [ "$current_shell" != "$HOME/.nix-profile/bin/zsh" ]; then
-      "$HOME/.nix-profile/bin/zsh" -c \
-      "chsh -s '$HOME/.nix-profile/bin/zsh' '$USER'"
-    fi
-  '';
 }
