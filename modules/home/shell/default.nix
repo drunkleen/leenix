eb69c0868f32258f7f176218ca499166056ac97f
DrunkLeen <snape@drunkleen.com>
2026-08-08 15:25:30 +0200

    feat: add zsh and starship home module

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -11,4 +11,14 @@
   programs.starship = {
     enable = true;
   };
+
+  home.activation.setDefaultShell =
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    current_shell="$(getent passwd "$USER" | cut -d: -f7)"
+
+    if [ "$current_shell" != "$HOME/.nix-profile/bin/zsh" ]; then
+      "$HOME/.nix-profile/bin/zsh" -c \
+      "chsh -s '$HOME/.nix-profile/bin/zsh' '$USER'"
+    fi
+  '';
 }
